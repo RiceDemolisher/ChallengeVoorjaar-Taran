d48d71dd671ac15b6d967a8988f89ed117456797
Taran Singh <taran.jhattu@gmail.com>
2019-03-26 12:26:17 +0100

    Opdracht 3

---
+++ K3/index.php
@@ -0,0 +1,29 @@
+<html>
+<body>
+<form method='post'>
+    <select name='subject[]' multiple size=5>
+        <option value='mercedes'>Mercedes</option>
+        <option value='ferrari'>Ferrari</option>
+        <option value='audi'>Audi</option>
+        <option value='ford'>Ford</option>
+        <option value='opel'>Opel</option>
+
+    </select>
+    <input type='submit' name='submit' value=Submit>
+</form>
+</body>
+</html>
+<?php
+
+
+if (isset($_POST["submit"])) {
+
+    if (isset($_POST["subject"])) {
+
+        foreach ($_POST['subject'] as $subject)
+
+            echo '<img src="img/' . $subject . '.jpg">';
+    } else
+        echo "Select an option first !!";
+}
+?>
